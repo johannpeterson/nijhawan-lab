0ba41d8cfad6416ff1019b8b1e8e315325e92d39
Johann Peterson <johann.peterson@gmail.com>
2020-04-26 11:50:09 -0700

    More sophisticated start_notebook.sh using .env

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,6 +1,37 @@
 #! /bin/bash
-DATA_DIR='/Volumes/home/bio'
-SOURCE_DIR=`pwd`
+set -e
+
+if [[ -f .env ]]; then
+    export $(cat .env | xargs)
+fi
+
+while getopts ":d:s:" opt; do
+    case ${opt} in
+        d )
+            DATA_DIR=$OPTARG
+            ;;
+        s )
+            SOURCE_DIR=$OPTARG
+            ;;
+        \? )
+            echo "Usage: start_notebook [-s sourcedir] [-d datadir]"
+             ;;
+        : )
+            echo "$OPTARG requires an argument." 1>&2
+            ;;
+    esac
+done
+
+if [[ -z "${DATA_DIR}" ]]; then
+    echo "Data directory must be specified with the DATA_DIR environment variable or with -d option."
+    exit 1
+fi
+
+if [[ -z "${SOURCE_DIR}" ]]; then
+    echo "Source directory must be specified with the SOURCE_DIR environment variable or with -s option."
+    exit 1
+fi
+
 docker run \
        -p 8888:8888 \
        --volume=$SOURCE_DIR:/home/bio/source \
