c242b036f35026f88a81ee98aca5aaf6892b0500
Johann Peterson <johann.peterson@gmail.com>
2021-09-27 17:14:16 -0700

    Added directory handling to MultiStream.py

--- source/jpbio/MultiStream.py
+++ source/jpbio/MultiStream.py
@@ -1,3 +1,5 @@
+import os
+
 class MultiStream:
 
     def __init__(self, tags=[], prefix='', suffix='', extension='.txt', mode='w', directory=''):
@@ -16,16 +18,24 @@ class MultiStream:
     def removed(self):
         return not self._finalizer.alive
 
-    def addTag(tag):
+    def addTag(self, tag):
         if tag not in self._streams:
             self.streams[tag] = None
     
-    def open():
+    def open(self):
         # TODO: use the directory argument
         for tag in self.streams:
-            filename = self.prefix + tag + self.suffix + self.extension
+            filename = os.path.join(
+                directory,
+                self.prefix + tag + self.suffix + self.extension)
             self.streams[tag] = open(filename, self.mode)
 
-    def close(stream_dict=self.streams):
+    def close(self, stream_dict=self.streams):
         for f in stream.dict:
             stream_dict[f].close()
+
+    def getStream(self, tag):
+        try:
+            return self._streams[tag]
+        except KeyError:
+            return None
