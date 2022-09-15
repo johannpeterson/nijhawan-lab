7514d6e15dc527ef241a7f94b89e7a1abd8d46ab
Johann Peterson <johann.peterson@gmail.com>
2022-09-14 18:13:08 -0700

    new util.py

--- source/jpbio/util.py
+++ source/jpbio/util.py
@@ -9,3 +9,26 @@ def rcDNA(seq):
 def cDNA(seq):
    return seq.translate(basePairTrans)
 
+def pad_list(l, length, element):
+    if len(l) < length:
+        l.extend( [element for _ in range(length - len(l))] )
+        return l
+    else:
+        return l
+
+# It is necessary to create lists of dictionaries differently from lists of integers,
+# because if you use the strategy above you get a list where every element is the same dict.
+def pad_dict_list(l, length):
+    if len(l) < length:
+        l.extend( [dict() for _ in range(length - len(l))] )
+        return l
+    else:
+        return l
+
+def export_figure(fig, title, metadata={}):
+    fig_metadata = {
+        "Comment" : "experiment:" + experiment
+    }
+    fig_metadata.update(metadata)
+    filename = data_directory + experiment + "_" + title + ".png"
+    fig.savefig(filename, metadata = fig_metadata)
