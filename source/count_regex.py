4960c9defa7675925d9987f873dc1a60eea2aa1f
Johann Peterson <johann.peterson@gmail.com>
2023-05-11 22:12:59 -0700

    Fixed regex.match vs regex.search issue

--- source/count_regex.py
+++ source/count_regex.py
@@ -2,7 +2,7 @@
 from Bio import Seq, SeqIO, Align
 import argparse, os
 #import gzip
-#import csv
+import csv
 import itertools
 import regex
 import importlib.util
@@ -14,20 +14,16 @@ try:
 except ImportError:  # Graceful fallback if IceCream isn't installed.
     ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
-#Yeschoices = ["true","t","yes","y"]
-#noChoices = ["false","f","no","n"]
-
 parser = argparse.ArgumentParser(description='Count regex matches in a FASTQ file.')
 parser.add_argument("pattern_file", help="Regex pattern strings in Python syntax.")
 parser.add_argument("FASTQ_file", help="FASTQ file to read")
-#parser.add_argument("primer_filename", help="Primers file.")
 parser.add_argument("-l", "--limit", help="Stop after reading LIMIT pairs of reads", type=int, default=None)
-#parser.add_argument("-r", "--rejected", help="Output read pairs that do not pass sanity checks (default no)", action="store_true")
-#parser.add_argument("-u", "--unassigned", help="Output read pairs that are not assigned to a group (default no)", action="store_true")
 parser.add_argument("-d", "--debug", help="Enable debugging output", action="store_true")
-#parser.add_argument("-q", "--quality", help="Minimum base call quality for matching regions (default 0).", type=int, default=0)
-#parser.add_argument("-r", "--rejected", help="Output read pairs that do not pass sanity checks (default no).", type=str.lower, choices=yesChoices + noChoices, default="no")
-#parser.add_argument("-u", "--unassigned", help="Output read pairs that are not assigned to a group (default no).", type=str.lower, choices=yesChoices + noChoices, default="no")
+parser.add_argument("-o", "--out", help="Write a table (tsv) of the named groups matching each line.", type=argparse.FileType('w'), default=None)
+parser.add_argument("-s", "--stats", help="Output statistics including match counts.", action="store_true")
+parser.add_argument("--seq", help="Write the sequences to stdout.", action="store_true")
+parser.add_argument("-v", "--verbose", help="Verbose - very.", action="store_true")
+
 args = parser.parse_args()
 
 if not isinstance(ic, types.FunctionType):
@@ -58,28 +54,54 @@ def main():
     ic(sequences.patterns)
 
     regexes = {k:regex.compile(sequences.patterns[k]) for k in sequences.patterns.keys()}
-    match_counts = {k:0 for k in sequences.patterns.keys() }
+    match_counts = {k:0 for k in sequences.patterns.keys()}
+    column_names = [group_name for r in regexes.values() for group_name in r.groupindex.keys()]
+    row_count=0
 
+    if args.out is not None:
+        ic('Write to the file {}'.format(args.out))
+        tableWriter = csv.DictWriter(
+            args.out,
+            fieldnames=column_names,
+            delimiter='\t',
+            restval='',
+            extrasaction='ignore'
+        )
+        tableWriter.writeheader()
+    else:
+        tableWriter = lambda x: None
+    
     ic(regexes)
     ic(match_counts)
     
     with open(args.FASTQ_file, "rt") as fq_file:
         reads1 = SeqIO.parse(fq_file, "fastq")
         for read in itertools.islice(reads1, args.limit):
-            match_groups = {group_name:'' for r in regexes.values() for group_name in list(r.groupindex.keys())}
+            match_groups = {name:None for name in column_names}
             seq = str(read.seq)
-            # print(seq)
+            if args.seq:
+                print(seq)
+            row_count += 1
             for label, r in regexes.items():
-                m = r.match(seq)
+                m = r.search(seq)
+                if args.verbose:
+                    ic(r)
                 if m:
                     match_counts[label] += 1
                     for group_name, group_value in m.groupdict().items():
                         match_groups[group_name] = group_value
-                        # print(group_name + ": " + group_value)
-            print(match_groups)
+            #ic(match_groups)
+            if args.out:
+                tableWriter.writerow(match_groups)
                 
+    if args.out is not None:
+        args.out.close()
 
-    print(match_counts)
+    if args.stats:
+        column_width = max( map(len, match_counts.keys())) + 2
+        print('{key:<{padding}}{value:>6}'.format(key='total rows', value=row_count, padding=column_width))
+        for k,v in match_counts.items():
+            print('{key:<{padding}}{value:>6}'.format(key=k, value=v, padding=column_width))
             
 if __name__ == '__main__':
     main()
