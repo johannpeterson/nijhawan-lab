7b759e39ce09d36ef3a82ff38bab824bcbf75af6
Johann Peterson <johann.peterson@gmail.com>
2023-06-04 12:06:22 -0700

    added infile & outfile arguments

--- source/template.py
+++ source/template.py
@@ -4,6 +4,7 @@
 """
 
 import types
+import sys
 import argparse
 
 try:
@@ -12,6 +13,12 @@ except ImportError:  # Graceful fallback if IceCream isn't installed.
     ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
 parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("infile", nargs='?',
+                    help="File to read from;  stdin if not provided.",
+                    type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument("outfile", nargs='?',
+                    help="File to write to;  stdout if not provided.",
+                    type=argparse.FileType('w'), default=sys.stdout)
 # parser.add_argument...
 
 args = parser.parse_args()
