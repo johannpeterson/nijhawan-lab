6444230a25c931e1f9b996809193d9bf9ce8b382
Johann Peterson <johann.peterson@gmail.com>
2023-06-10 10:56:27 -0700

    trim_barcodes as pipe or reads argument

--- source/trim_barcodes.sh
+++ source/trim_barcodes.sh
@@ -1,6 +1,15 @@
 #!/bin/zsh
 
-csvtk cut -t -f fwd_primer,rev_primer,barcode < $1 \
+if [[ $# -gt 0 ]]; then
+    # File name provided as an argument
+    file_path=$1
+else
+    # Read from standard input
+    file_path="/dev/stdin"
+fi
+
+cat "$file_path" \
+    | csvtk cut -t -f fwd_primer,rev_primer,barcode \
     | csvtk -t filter2 -t -f 'len($barcode)<30' \
     | csvtk sort -t -k fwd_primer -k rev_primer -k barcode \
     | datamash -H -g fwd_primer,rev_primer,barcode count barcode \
