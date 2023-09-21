33b7fd90a3d3c6dfd1771a40389ae5a7474a0bd4
Johann Peterson <johann.peterson@gmail.com>
2023-09-20 18:21:31 -0700

    Added final join & count steps.

--- source/barcodes.sh
+++ source/barcodes.sh
@@ -1,13 +1,21 @@
 #!/bin/zsh
 
 SOURCEDIR=~/bio/nijhawanlab/source
+
+# Required input files:
 MERGED_READS=$1
-PRIMERS=primers.txt
-PATTERNS=patterns.py
 BARCODE_PATTERNS=barcode_patterns.py
+PRIMERS=primers.txt
+SAMPLES=samples.tsv
+
+# Intermediate and result files:
+PATTERNS=patterns.py
 MATCHES=matches.tsv
 BARCODE_TABLE=barcode_table.tsv
 BARCODE_COUNTS=barcode_counts.tsv
+BARCODE_COUNTS_SAMPLES=barcode_counts_samples.tsv
+FLAT_SAMPLES=flat_samples.tsv
+COUNTS=totals_by_sample.tsv
 
 echo "get_regexes.py"
 echo "separate_matchtable.awk"
@@ -22,3 +30,12 @@ $SOURCEDIR/join_matches.sh < $MATCHES | \
 
 echo "Compute barcode counts"
 $SOURCEDIR/count_barcodes.sh < $BARCODE_TABLE > $BARCODE_COUNTS
+
+echo "Flatten samples table"
+$SOURCEDIR/flatten_samples.py $SAMPLES $FLAT_SAMPLES
+
+echo "Join with sample names"
+csvtk join -t --left-join -f "fwd_primer,rev_primer" $BARCODE_COUNTS $FLAT_SAMPLES > $BARCODE_COUNTS_SAMPLES
+
+echo "Compute totals for each sample."
+csvtk summary -t -g sample,fwd_primer,rev_primer -f frequency:sum $BARCODE_COUNTS_SAMPLES > $COUNTS
