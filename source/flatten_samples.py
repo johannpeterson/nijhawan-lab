38fdecf14cf97ba15a77733715161837ae576d2e
Johann Peterson <johann.peterson@gmail.com>
2023-09-20 17:38:33 -0700

    Infers primer name prefix

--- source/flatten_samples.py
+++ source/flatten_samples.py
@@ -10,6 +10,7 @@ import sys
 import argparse
 import re
 import csv
+import itertools
 
 try:
     from icecream import ic
@@ -26,7 +27,7 @@ parser.add_argument("infile", nargs='?',
 parser.add_argument("outfile", nargs='?',
                     help="File to write to;  stdout if not provided.",
                     type=argparse.FileType('w'), default=sys.stdout)
-parser.add_argument('--prefix', '-p', default='oVK',
+parser.add_argument('--prefix', '-p', default=None,
                     help="Primer name prefix, e.g. oVK.")
 parser.add_argument('--all', '-a', action='store_true', default=False,
                     help="Write a row for every pair of primers, even if the name is blank.")
@@ -44,18 +45,45 @@ if not isinstance(ic, types.FunctionType):
         ic.disable()
 ic(args)
 
-label_prefix = args.prefix
-primer_name_regex_string = r"(" + label_prefix + ")(\d{1,3})"
-primer_name_regex = re.compile(primer_name_regex_string)
 
-def pad_primer(s):
+def pad_primer(s, l, primer_name_regex):
     m = primer_name_regex.match(s)
     if m is not None:
-      return m.group(1) + m.group(2).rjust(3, '0')
+      return m.group(1) + m.group(2).rjust(l, '0')
     else:
       return None
 
   
+def all_equal(l):
+    return all(i==l[0] for i in l)
+
+
+def common_prefix(string_list):
+    return ''.join(map(lambda l: l[0], itertools.takewhile(all_equal, zip(*string_list))))
+
+
+def take_prefix(s):
+    r=re.compile('^(.*?)[0-9]+$')
+    m = r.match(s)
+    if m:
+        return m.group(1)
+    else:
+        return ''
+
+    
+def split_primer_name(s):
+    r = re.compile('^(.*?)([0-9]+)$')
+    m = r.match(s)
+    if m:
+        return (m.group(1), m.group(2))
+    else:
+        return ('','')
+
+
+def infer_prefix(primer_list):
+    return common_prefix(primer_list)
+
+
 # ----------------------------------------------------------
 # main
 # ----------------------------------------------------------
@@ -65,21 +93,38 @@ def main():
     """main
     """
     csvraw = list(csv.reader(args.infile, delimiter='\t'))
-    col_headers = [pad_primer(h) for h in csvraw[0][1:]]
-    row_headers = [pad_primer(row[0]) for row in csvraw[1:]]
+    # ic(csvraw)
+    col_headers = csvraw[0][1:]
+    row_headers = [r[0] for r in csvraw[1:]]
+    ic(col_headers, row_headers)
+    split_list = list(map(split_primer_name, col_headers + row_headers))
+    prefixes = [i[0] for i in split_list]
+    suffixes = [i[1] for i in split_list]
+    ic(prefixes, suffixes)
+    suffix_length = max(map(len, suffixes))
+    label_prefix = infer_prefix(prefixes)
+    ic(suffix_length)
+    ic(label_prefix)
+    
+    primer_name_regex_string = r"(" + label_prefix + ")(\d+)"
+    primer_name_regex = re.compile(primer_name_regex_string)
+
+    col_headers = [pad_primer(h, suffix_length, primer_name_regex) for h in col_headers]
+    row_headers = [pad_primer(h, suffix_length, primer_name_regex) for h in row_headers]
+    ic(col_headers, row_headers)
     data = [row[1:] for row in csvraw[1:]]
     sample_list = [s for sublist in data for s in sublist if s != '']
 
     sample_dict = {row_headers[r]:{col_headers[c]:'' for c in range(len(col_headers))}
                    for r in range(0, len(row_headers))}
-        
+
     for r in range(len(row_headers)):
         for c in range(0,len(col_headers)):
             try:
                 sample_dict[row_headers[r]][col_headers[c]]=data[r][c]
             except:
                 pass
-                
+
     writer = csv.writer(args.outfile, delimiter='\t', lineterminator='\n')
     if not args.noheader:
         writer.writerow(['fwd_primer','rev_primer','sample'])
