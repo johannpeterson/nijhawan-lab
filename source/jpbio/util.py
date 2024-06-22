21b390c47e523722aeb1ef81d2946a13b3d21b09
Johann Peterson <johann.peterson@gmail.com>
2024-06-22 13:37:13 -0700

    doctrings in util.py

--- source/jpbio/util.py
+++ source/jpbio/util.py
@@ -1,20 +1,24 @@
 # jpbio/util.py
+"""Utility functions for bioinformatics scripts."""
 
 import regex
 
-basePair = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+basePair = {"A": "T", "T": "A", "G": "C", "C": "G"}
 basePairTrans = str.maketrans(basePair)
 
 
 def rcDNA(seq):
+    """Return the reverse DNA complement."""
     return seq.translate(basePairTrans)[::-1]
 
 
 def cDNA(seq):
+    """Return the DNA complement."""
     return seq.translate(basePairTrans)
 
 
 def pad_list(item_list, length, element):
+    """Return item_list extended to be at least length long."""
     if len(item_list) < length:
         item_list.extend([element for _ in range(length - len(item_list))])
         return item_list
@@ -22,11 +26,15 @@ def pad_list(item_list, length, element):
         return item_list
 
 
-# It is necessary to create lists of dictionaries differently
-# from lists of integers,
-# because if you use the strategy above you get a list where
-# every element is the same dict.
 def pad_dict_list(item_list, length):
+    """Return of list of at least length dict objects.
+
+    Extend item_list, if necessary, by creating new empty dicts.
+
+    It is necessary to create lists of dictionaries differently
+    from lists of integers, because if you use the strategy above
+    you get a list where every element is the same dict.
+    """
     if len(item_list) < length:
         item_list.extend([dict() for _ in range(length - len(item_list))])
         return item_list
@@ -35,19 +43,20 @@ def pad_dict_list(item_list, length):
 
 
 def export_figure(fig, title, data_directory, experiment, metadata={}):
-    fig_metadata = {
-        "Comment": "experiment:" + experiment
-    }
+    """Export a figure to PNG file using a standardized file name."""
+    fig_metadata = {"Comment": "experiment:" + experiment}
     fig_metadata.update(metadata)
     filename = data_directory + experiment + "_" + title + ".png"
     fig.savefig(filename, metadata=fig_metadata)
 
 
 def sequence_to_regex(seq, group=True, name=None):
-    # Return a regular expression string to match a DNA sequence.
-    # 'N' is changed to '(.)', and a series of consecutive 'N' is changed
-    # to '(.{k})', where k is the number of Ns.
-    nRegex = regex.compile('N{2,}')
+    """Return a regular expression string to match a DNA sequence.
+
+    'N' is changed to '(.)', and a series of consecutive 'N' is changed
+    to '(.{k})', where k is the number of Ns.
+    """
+    nRegex = regex.compile("N{2,}")
     matches = nRegex.finditer(seq)
     marker = 0
     newSeq = ""
@@ -56,14 +65,13 @@ def sequence_to_regex(seq, group=True, name=None):
         group_length = m.span()[1] - m.span()[0]
         if name:
             group_string = "(?P<{name}{num}>.{{{len}}})".format(
-                name=name,
-                num=group_num,
-                len=group_length)
+                name=name, num=group_num, len=group_length
+            )
         elif group:
             group_string = "(.{{{len}}})".format(len=group_length)
         else:
             group_string = ".{{{len}}}".format(len=group_length)
-        newSeq += seq[marker:m.span()[0]] + group_string
+        newSeq += seq[marker : m.span()[0]] + group_string
         marker = m.span()[1]
         group_num += 1
         newSeq += seq[marker:]
@@ -71,24 +79,28 @@ def sequence_to_regex(seq, group=True, name=None):
 
 
 def common_sequence(seqs):
-    # Return a string representing the common sequence of a list of sequences.
-    # Letters in the returned string are either A, C, T or G
-    # if the letter in that position is the same in all sequences,
-    # or N if not.  Sequences must be the same length.
+    """Return a string representing the common sequence of a list of sequences.
+
+    Letters in the returned string are either A, C, T or G
+    if the letter in that position is the same in all sequences,
+    or N if not.  Sequences must be the same length.
+    """
 
     def elements_equal(element_list):
         if all([x == element_list[0] for x in element_list]):
             return element_list[0]
         else:
-            return 'N'
+            return "N"
 
     N = max(map(len, seqs))
 
-    return ''.join(list(map(elements_equal,
-                            [[l[i] for l in seqs] for i in range(N)])))
+    return "".join(
+        list(map(elements_equal, [[seq[i] for seq in seqs] for i in range(N)]))
+    )
 
 
 def hamming_distance(s1, s2):
+    """Return the Hamming distance between two strings of equal length."""
     if len(s1) != len(s2):
         raise ValueError("hamming_distance: string arguments must have the same length")
     return [s1[i] != s2[i] for i in range(len(s1))]
